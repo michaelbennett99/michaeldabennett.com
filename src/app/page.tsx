490dbe280a80b10afa1f7836d88559266d1b5ed3
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-09 20:55:02 +0000

    initialise homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,114 @@
-import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="divide-solid divide-y divide-gray-200 mx-8 lg:mx-auto lg:max-w-4xl">
+      <Header />
+      <AboutMe />
+      <Projects />
+    </main>
+  );
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+function Header() {
+  return (
+    <header className="pt-16 pb-8">
+      <h1 className="text-8xl font-bold text-center">Michael Bennett</h1>
+      <h3 className="text-4xl text-center">Software Engineer</h3>
+      <nav className="my-2 flex justify-center gap-4">
+        <SocialLink
+          href="https://github.com/michaelbennett99"
+          icon={<FaGithub />}
+        />
+        <SocialLink
+          href="https://www.linkedin.com/in/michaeldabennett"
+          icon={<FaLinkedin />}
+        />
+      </nav>
+    </header>
+  );
+}
+
+function SocialLink({
+  href,
+  icon,
+}: {
+  href: string;
+  icon: React.ReactNode;
+}) {
+  return (
+    <Link href={href} className="text-4xl text-gray-700 hover:text-gray-900">
+      {icon}
+    </Link>
+  );
+}
+
+function Subsection({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="my-8 mx-auto px-8 max-w-3xl box-content">
+      <h2 className="text-4xl font-bold text-center my-8">{title}</h2>
+      {children}
+    </section>
+  );
+}
+
+function AboutMe() {
+  return (
+    <Subsection title="About Me">
+      <p className="text-2xl text-justify">
+        I am a software engineer with a passion for building products that
+        make a difference.
+      </p>
+    </Subsection>
+  );
+}
+
+function Projects() {
+  return (
+    <Subsection title="Projects">
+      <ul className="flex flex-col justify-center gap-4">
+        <li key="project-1">
+          <ProjectCard
+            name="Project 1"
+            url="https://www.google.com"
+            githubUrl="https://www.github.com"
+            description="This is a project"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </li>
+      </ul>
+    </Subsection>
+  )
+}
+
+function ProjectCard({
+  name,
+  url,
+  githubUrl,
+  description,
+}: {
+  name: string;
+  url?: string;
+  githubUrl?: string;
+  description: string;
+}) {
+  return (
+    <div className="flex flex-col">
+      <h3 className="text-2xl font-bold">
+        {
+          url
+          && <Link href={url} className="text-lg">{name}</Link>
+          || <span className="text-lg">{name}</span>
+        }
+      </h3>
+      {githubUrl && <SocialLink href={githubUrl} icon={<FaGithub />} />}
+      <p className="text-lg">{description}</p>
     </div>
   );
 }
