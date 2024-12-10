bae67e9718c5bdef74100e1e2b8d82f915e43ec2
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 17:34:02 +0000

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { LuMoon, LuSun } from "react-icons/lu";
 import { ThemeProvider, useTheme } from "next-themes";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -45,6 +46,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
@@ -134,5 +136,60 @@ function DarkModeToggle({ className }: { className?: string }) {
         : <LuSun />
       }
     </button>
+  );
+}
+
+function Footer() {
+  const { theme } = useTheme();
+
+  return (
+    <footer className="text-muted-foreground bg-muted border-t text-sm py-4">
+      <div
+        className="flex flex-row justify-around items-center"
+      >
+        <div>
+          {
+            theme === "dark"
+            ? <Image
+              src="/images/logo.white.png"
+              alt="Michael Bennett"
+              width={100}
+              height={100}
+              className="rounded-full"
+            />
+            : <Image
+              src="/images/logo.black.png"
+              alt="Michael Bennett"
+              width={100}
+              height={100}
+              className="rounded-full"
+            />
+          }
+        </div>
+        <div>
+          <div className="text-center">
+            <span className="font-bold">Michael Bennett </span>
+            <span className="text-muted-foreground">
+              &copy; 2024
+            </span>
+          </div>
+          <div>
+            Built with <a
+              href="https://nextjs.org"
+              target="_blank"
+              className="text-primary hover:text-primary-foreground"
+            >
+              Next.js
+            </a> and <a
+              href="https://tailwindcss.com"
+              target="_blank"
+              className="text-primary hover:text-primary-foreground"
+            >
+              Tailwind CSS
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
