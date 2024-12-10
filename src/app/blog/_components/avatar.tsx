40539b9967b7d3c2c6655579cea7a3ef8a07bd10
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 21:57:20 +0000

    create avatar

---
+++ src/app/blog/_components/avatar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+type Props = {
+  name: string;
+  picture: string;
+}
+
+export default function Avatar({ name, picture }: Props) {
+  return <div className="flex items-center">
+    <Image
+      src={picture}
+      className="w-12 h-12 rounded-full mr-4"
+      alt={name}
+      width={48}
+      height={48}
+    />
+    <div className="text-xl font-bold">{name}</div>
+  </div>;
+}
