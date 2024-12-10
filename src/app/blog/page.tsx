05493f6139e353934b32a42520a9394b5dcfafae
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 22:21:38 +0000

    add container to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,7 @@ import { getAllPosts } from "@/lib/api";
 import { CMS_NAME } from "@/lib/constants";
 import HeroPost from "./_components/hero_post";
 import MorePosts from "./_components/more_posts";
+import Container from "../_components/container";
 
 export default function Blog() {
   const allPosts = getAllPosts();
@@ -10,27 +11,29 @@ export default function Blog() {
 
   return (
     <main>
-      <section>
-        <h1 className="text-center text-2xl font-bold">Blog</h1>
-        <h4 className="text-center text-lg mt-5">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
-      </h4>
-      </section>
-      <HeroPost
-        title={heroPost.title}
-        date={heroPost.date}
-        author={heroPost.author}
-        slug={heroPost.slug}
-        excerpt={heroPost.excerpt}
-      />
-      {morePosts.length > 0 && <MorePosts posts={morePosts} />}
+      <Container>
+        <section>
+          <h1 className="text-center text-2xl font-bold">Blog</h1>
+          <h4 className="text-center text-lg mt-5">
+            A statically generated blog example using{" "}
+            <a
+              href="https://nextjs.org/"
+              className="underline hover:text-blue-600 duration-200 transition-colors"
+            >
+              Next.js
+            </a>{" "}
+            and {CMS_NAME}.
+          </h4>
+        </section>
+        <HeroPost
+          title={heroPost.title}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+        {morePosts.length > 0 && <MorePosts posts={morePosts} />}
+      </Container>
     </main>
   );
 }
