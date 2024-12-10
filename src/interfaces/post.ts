ff385eb1fd71ecc20c3b7d1f0b515ab8c9922161
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 17:52:03 +0000

    add interfaces for blog

---
+++ src/interfaces/post.ts
@@ -0,0 +1,12 @@
+import type { Author } from "./author";
+
+export type Post = {
+    slug: string;
+    title: string;
+    date: string;
+    author: Author;
+    coverImage?: string;
+    excerpt?: string;
+    content: string;
+    preview?: string
+}
