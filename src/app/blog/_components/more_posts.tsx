c398be50a59546fdc1e408ba18d8cf01e85bd391
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 22:07:07 +0000

    add more_posts

---
+++ src/app/blog/_components/more_posts.tsx
@@ -0,0 +1,21 @@
+import { Post } from "@/interfaces/post";
+import PostPreview from "@/app/blog/_components/post_preview";
+
+type Props = {
+  posts: Post[];
+}
+
+export default function MorePosts({ posts }: Props) {
+  return <section>
+      <h2
+        className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight"
+      >
+        More Posts
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+        {posts.map((post) => (
+          <PostPreview key={post.slug} {...post} />
+        ))}
+      </div>
+  </section>;
+}
