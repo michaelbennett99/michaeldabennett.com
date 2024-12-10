1fae0b90da185ff95bb43cb5e58a1550e09ee9a4
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 17:03:10 +0000

    path to cargo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,6 +106,7 @@ function Projects() {
         <li key="rusty_chess_clock">
           <ProjectCard
             name="Rusty Chess Clock"
+            url="https://crates.io/crates/rusty_chess_clock"
             githubUrl="https://github.com/michaelbennett99/rusty_chess_clock"
             description="Customisable chess clock GUI (using iced-rs) and CLI (using termion) application implemented in Rust, backed by a zero-dependency library providing a chess clock API. Built as a quick rust exercise to practice the language and a slightly lengthier exercise to learn one method for building a GUI application."
           />
