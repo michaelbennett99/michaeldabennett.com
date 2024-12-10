581d0a5500e9f4d07a4bdec709444c5219c0dd75
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 12:06:33 +0000

    does commiting help

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
 import { twJoin } from "tailwind-merge";
 import { useDarkMode } from "usehooks-ts";
 import { LuMoon, LuSun } from "react-icons/lu";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,10 +35,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { isDarkMode, toggle } = useDarkMode({
-    defaultValue: window.matchMedia("(prefers-color-scheme: dark)").matches,
+  const { isDarkMode, toggle, enable, disable } = useDarkMode({
+    defaultValue: false,
     initializeWithValue: false
   });
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
+    if (darkModeMediaQuery.matches) {
+      enable();
+    } else {
+      disable();
+    }
+  }, []);
 
   return (
     <html lang="en" className={isDarkMode ? "dark" : ""}>
@@ -49,12 +63,14 @@ export default function RootLayout({
       <body
         className={twJoin(
           `${geistSans.variable} ${geistMono.variable} antialiased`,
-          "bg-white text-gray-950 border-gray-200",
-          "dark:bg-gray-900 dark:text-gray-300 dark:border-gray-800"
         )}
       >
         <header
-          className="text-lg text-bold sticky top-0 border-b border-inherit flex flex-row justify-between items-center"
+          className={twJoin(
+            "text-lg text-bold sticky top-0 border-b",
+            "bg-background text-foreground border-border",
+            "flex flex-row justify-between items-center"
+          )}
         >
           <Link href="/" className="px-8 lg:px-16">
             <span className="text-4xl font-bold">
@@ -64,15 +80,16 @@ export default function RootLayout({
           <nav
             className="flex flex-row justify-around items-stretch"
           >
-            <NavLink href="/about">About</NavLink>
-            <NavLink href="/cv">CV</NavLink>
-            <NavLink href="/blog">Blog</NavLink>
-            <NavLink href="/contact">Contact</NavLink>
+            <NavLink href="/about" pathname={pathname}>About</NavLink>
+            <NavLink href="/cv" pathname={pathname}>CV</NavLink>
+            <NavLink href="/blog" pathname={pathname}>Blog</NavLink>
+            <NavLink href="/contact" pathname={pathname}>Contact</NavLink>
             <DarkModeToggle
               isDarkMode={isDarkMode}
               toggle={toggle}
               className={twJoin(
-                "px-6 hover:text-gray-800 hover:bg-gray-200",
+                "px-6 text-foreground bg-background",
+                "hover:text-muted-foreground hover:bg-muted",
                 "transition-colors duration-300"
               )}
             />
@@ -87,18 +104,22 @@ export default function RootLayout({
 function NavLink({
   children,
   href,
+  pathname
 }: {
   children: React.ReactNode;
   href: string;
+  pathname: string;
 }) {
+  const isActive = pathname.endsWith(href);
+
   return (
     <Link
       href={href}
       className={twJoin(
-        "text-lg font-bold py-4",
+        "text-lg font-bold py-4 px-1 md:px-2 lg:px-4 2xl:px-8",
+        isActive ? "!text-gray-800 !bg-gray-200" : "",
         "hover:text-gray-800 hover:bg-gray-200",
-        "transition-colors duration-300 flex-grow text-center border-inherit",
-        "px-1 md:px-2 lg:px-4 2xl:px-8"
+        "transition-colors duration-300 flex-grow text-center",
       )}
     >
       {children}
@@ -121,8 +142,8 @@ function DarkModeToggle(
     <button onClick={toggle} className={className}>
       {
         isDarkMode
-        ? <LuMoon />
-        : <LuSun />}
+        ? <LuMoon className="text-gray-800 bg-gray-200" />
+        : <LuSun className="text-gray-800 bg-gray-200" />}
     </button>
   );
 }
