06056f61665ca6538ee911041233f5741d6a7de0
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 22:19:27 +0000

    add container

---
+++ src/app/_components/container.tsx
@@ -0,0 +1,13 @@
+import { cn } from '@/lib/utils'
+
+export default function Container(
+  { children, className }: { children: React.ReactNode, className?: string }
+) {
+  return (
+    <div
+      className={cn("mx-8 lg:mx-auto lg:max-w-4xl", className)}
+    >
+      {children}
+    </div>
+  );
+}
