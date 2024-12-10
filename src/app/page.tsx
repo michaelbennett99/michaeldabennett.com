3851ee2cf62e83992fcb3e3e6fa1bfd6d22f4a40
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-10 17:00:09 +0000

    add rusty_chess_clock project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,12 +95,19 @@ function Projects() {
   return (
     <Subsection title="Projects">
       <ul className="flex flex-col justify-center gap-4">
-        <li key="project-1">
+        <li key="stagehunter">
           <ProjectCard
             name="StageHunter"
             url="https://stagehunter.cc"
             githubUrl="https://github.com/michaelbennett99/stagehunter"
-            description="A daily web game that tests users’ pro cycling knowledge. Built using Typescript and Next.js (frontend), Go (backend), Postgres (database), AWS (hosting) and Cloudflare (DNS)."
+            description="A daily web game that tests users’ pro cycling knowledge. Built using Typescript and Next.js (frontend), Go (backend), Postgres + PostGIS (database), AWS (hosting) and Cloudflare (DNS)."
+          />
+        </li>
+        <li key="rusty_chess_clock">
+          <ProjectCard
+            name="Rusty Chess Clock"
+            githubUrl="https://github.com/michaelbennett99/rusty_chess_clock"
+            description="Customisable chess clock GUI (using iced-rs) and CLI (using termion) application implemented in Rust, backed by a zero-dependency library providing a chess clock API. Built as a quick rust exercise to practice the language and a slightly lengthier exercise to learn one method for building a GUI application."
           />
         </li>
       </ul>
