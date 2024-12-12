e566101cdd245eb07227daf7c2fd55b18efee08b
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-12 12:24:50 +0000

    incorporate navButton element into dark_mode_toggle

--- src/app/_components/layout/header.tsx
+++ src/app/_components/layout/header.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import NavLink from "@/components/nav_link";
 import DarkModeToggle from "@/app/_components/layout/dark_mode_toggle";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { LuMenu } from "react-icons/lu";
+import NavButton from "./nav_button";
 
 export default function Header() {
   const pathname = usePathname();
@@ -24,17 +27,20 @@ export default function Header() {
       <nav
         className="flex flex-row justify-end h-full"
       >
-        <NavLink href="/about" pathname={pathname}>About</NavLink>
-        <NavLink href="/cv" pathname={pathname}>CV</NavLink>
-        <NavLink href="/blog" pathname={pathname}>Blog</NavLink>
-        <NavLink href="/contact" pathname={pathname}>Contact</NavLink>
-        <DarkModeToggle
-          className={cn(
-            "px-2 md:px-4 lg:px-8",
-            "transition-colors duration-300",
-            "hover:bg-accent hover:text-accent-foreground"
-          )}
-        />
+        <div className="hidden md:flex flex-row">
+          <NavLink href="/about" pathname={pathname}>About</NavLink>
+          <NavLink href="/cv" pathname={pathname}>CV</NavLink>
+          <NavLink href="/blog" pathname={pathname}>Blog</NavLink>
+          <NavLink href="/contact" pathname={pathname}>Contact</NavLink>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <NavButton className="md:hidden">
+              <LuMenu />
+            </NavButton>
+          </DropdownMenuTrigger>
+        </DropdownMenu>
+        <DarkModeToggle />
       </nav>
     </header>
   )
