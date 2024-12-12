70206fb58cdf0d60ad00d885e6e57a9f4455d04d
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-12 20:09:53 +0000

    Refactor header component to use NavBar; create new NavBar component for navigation links

---
+++ src/app/_components/layout/nav_bar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import NavLink from "@/components/nav_link";
+import NavBarDropdown from "./nav_bar_dropdown";
+
+export default function NavBar() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex flex-row h-full">
+      <div className="hidden md:flex flex-row">
+        {links.map((link) => (
+          <NavLink
+            href={link.href}
+            pathname={pathname}
+            key={link.href}
+          >
+            {link.label}
+          </NavLink>
+        ))}
+      </div>
+      <NavBarDropdown links={links} />
+    </nav>
+  )
+}
+
+const links = [
+  { href: "/about", label: "About" },
+  { href: "/cv", label: "CV" },
+  { href: "/blog", label: "Blog" },
+  { href: "/contact", label: "Contact" },
+]
