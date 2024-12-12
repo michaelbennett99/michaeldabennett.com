f81bee4d2b30f0ccb82279a077aae335ea72ad45
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-12 19:38:00 +0000

    refactor nav bar dropdown for mobile

--- src/app/_components/layout/header.tsx
+++ src/app/_components/layout/header.tsx
@@ -1,19 +1,12 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-import { LuMenu } from "react-icons/lu";
 
 import { cn } from "@/lib/utils";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+
 import DarkModeToggle from "@/app/_components/layout/dark_mode_toggle";
-import NavButton from "./nav_button";
 import NavLink from "@/components/nav_link";
-import DropdownLink from "@/components/dropdown_link";
+import NavBarDropdown from "./nav_bar_dropdown";
 
 export default function Header() {
   const pathname = usePathname();
@@ -45,20 +38,7 @@ export default function Header() {
             </NavLink>
           ))}
         </div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <NavButton className="md:hidden">
-              <LuMenu />
-            </NavButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            {links.map((link) => (
-              <DropdownMenuItem key={link.href}>
-                <DropdownLink href={link.href}>{link.label}</DropdownLink>
-              </DropdownMenuItem>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <NavBarDropdown links={links} />
         <DarkModeToggle />
       </nav>
     </header>
