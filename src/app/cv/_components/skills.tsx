f5f7b6e4eb3e41e0d0d175facb2a809566a3324a
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-12 19:49:33 +0000

    factor out skill

--- src/app/cv/_components/skills.tsx
+++ src/app/cv/_components/skills.tsx
@@ -15,58 +15,9 @@ import {
   SiGnubash
 } from "react-icons/si";
 import { FaAws } from "react-icons/fa";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { cn } from "@/lib/utils";
+
 import CardBody from "./card_body";
-
-type Proficiency = "Beginner" | "Intermediate" | "Advanced";
-
-function Skill({
-  icon,
-  name,
-  proficiency,
-}: {
-  icon: React.ReactNode;
-  name: string;
-  proficiency: Proficiency;
-}) {
-  const proficiency_color = {
-    Beginner: "text-red-600",
-    Intermediate: "text-green-600",
-    Advanced: "text-purple-600",
-  }[proficiency];
-
-  return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <div
-            className={cn(
-              "flex flex-row justify-left items-center gap-2",
-              proficiency_color
-            )}
-            aria-label={`${name} proficiency: ${proficiency}`}
-          >
-            {icon}
-            <p>{name}</p>
-          </div>
-        </TooltipTrigger>
-        <TooltipContent
-          className="bg-muted text-muted-foreground"
-          side="right"
-          sideOffset={10}
-        >
-          <p>{`Proficiency: ${proficiency}`}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  );
-}
+import Skill from "./skill";
 
 export default function Skills() {
   return (
