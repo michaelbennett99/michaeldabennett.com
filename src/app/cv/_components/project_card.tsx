17751963775d5c227fccf37cb993caa2d455a2f4
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-12 15:38:30 +0000

    add cv project card

---
+++ src/app/cv/_components/project_card.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+
+import CardHeader from "./card_header";
+import CardBody from "./card_body";
+
+export default function ProjectCard({
+  name,
+  url,
+  githubUrl,
+  description,
+}: {
+  name: string;
+  url?: string;
+  githubUrl?: string;
+  description: string;
+}) {
+  return (
+    <div className="space-y-2">
+      <CardHeader>
+        <div className="flex flex-row justify-left items-center gap-2">
+          <h3 className="font-bold">
+            { url
+              && <Link href={url} className="hover:underline">{name}</Link>
+              || <span>{name}</span>
+            }
+          </h3>
+          { githubUrl && <Link href={githubUrl}><FaGithub/></Link> }
+        </div>
+      </CardHeader>
+      <CardBody>
+        <p>{description}</p>
+      </CardBody>
+    </div>
+  );
+}
