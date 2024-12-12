588a93677d7acb36e117575bc50a8a810fa855cd
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-12 11:55:34 +0000

    create experience card

---
+++ src/app/cv/_components/experience_card.tsx
@@ -0,0 +1,43 @@
+import Bullets from "./bullets";
+
+type Props = {
+  title: string;
+  employer: string;
+  location: string;
+  start_date: string;
+  end_date: string;
+  description: string;
+  bullets: string[];
+};
+
+export default function ExperienceCard({
+  title,
+  employer,
+  location,
+  start_date,
+  end_date,
+  description,
+  bullets,
+}: Props) {
+  return (
+    <div className="flex flex-col">
+      <header className="flex flex-col">
+        <div className="flex flex-row justify-between">
+          <h3>
+            <strong>{title}</strong>
+          </h3>
+          <p>
+            {start_date} - {end_date}
+          </p>
+        </div>
+        <p className="text-sm">
+          <strong>{employer}, {location}</strong>
+        </p>
+      </header>
+      <main className="flex flex-col text-sm text-muted-foreground">
+        <p>{description}</p>
+        <Bullets bullets={bullets} />
+      </main>
+    </div>
+  );
+}
