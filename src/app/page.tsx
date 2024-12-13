327cff119fd030f420b8f07ed5cf9eb82bfa69ed
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-13 12:09:29 +0000

    Refactor typography and layout in main components for improved readability and consistency
    - Updated text classes in the Home, AboutMe, and ProjectCard components to enhance typography.
    - Adjusted margins and padding in the Subsection component for better spacing and layout.
    - Removed unnecessary text styling for a cleaner presentation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Projects from "./_components/page/projects";
 export default function Home() {
   return (
     <main>
-      <Container className="divide-solid divide-y">
+      <Container className="divide-solid divide-y text-lg mx-6">
         <Header />
         <AboutMe />
         <Projects />
