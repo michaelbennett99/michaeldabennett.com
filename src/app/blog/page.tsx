a88fd7b228a3ebc54928f1ee1bbbb5f10c72d5bf
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-13 11:01:25 +0000

    Remove CMS_NAME constant and related blog description from Blog page component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,4 @@
 import { getAllPosts } from "@/lib/api";
-import { CMS_NAME } from "@/lib/constants";
 import HeroPost from "./_components/hero_post";
 import MorePosts from "./_components/more_posts";
 import Container from "@/components/container";
@@ -15,16 +14,6 @@ export default function Blog() {
       <Container>
         <section>
           <PageHeading title="Blog" />
-          <h4 className="text-center text-lg mt-5">
-            A statically generated blog example using{" "}
-            <a
-              href="https://nextjs.org/"
-              className="underline hover:text-blue-600 duration-200 transition-colors"
-            >
-              Next.js
-            </a>{" "}
-            and {CMS_NAME}.
-          </h4>
         </section>
         <HeroPost
           title={heroPost.title}
