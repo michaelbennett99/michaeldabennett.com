26859c3d9f8a2a10f653275a40407fe203b8c0d4
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-13 11:31:50 +0000

    Refactor Section and Container components for improved layout
    - Removed unnecessary margin from Section component for cleaner design.
    - Updated Container component to adjust horizontal margins for better responsiveness across devices.

--- src/app/cv/_components/section.tsx
+++ src/app/cv/_components/section.tsx
@@ -7,7 +7,7 @@ export default function Section({
 }) {
   return (
     <section>
-      <div className="mx-8 my-4">
+      <div className="my-4">
         <h2 className="text-2xl font-bold">{title}</h2>
         <div className="my-4 space-y-4">{children}</div>
       </div>
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -5,7 +5,7 @@ export default function Container(
 ) {
   return (
     <div
-      className={cn("mx-2 md:mx-8 lg:mx-auto lg:max-w-4xl mb-16", className)}
+      className={cn("mx-8 md:mx-16 lg:mx-auto lg:max-w-4xl mb-16", className)}
     >
       {children}
     </div>
