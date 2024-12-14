2a3d53ee482b10bff9590ee2460e81306556b592
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-14 21:29:51 +0000

    add id to sections

--- src/app/cv/_components/section.tsx
+++ src/app/cv/_components/section.tsx
@@ -6,7 +6,7 @@ export default function Section({
   children: React.ReactNode;
 }) {
   return (
-    <section>
+    <section id={title.toLowerCase().replace(/\s+/g, "_")}>
       <div className="my-4">
         <h2 className="text-2xl font-bold">{title}</h2>
         <div className="my-4 space-y-4">{children}</div>
