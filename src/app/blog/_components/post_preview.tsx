b98088e269a45532755e0380a8fb4ef18aebed5c
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-14 21:35:45 +0000

    add id elements

--- src/app/blog/_components/post_preview.tsx
+++ src/app/blog/_components/post_preview.tsx
@@ -16,7 +16,7 @@ export default function PostPreview(
   { title, date, excerpt, author, slug }: Props
 ) {
   return (
-    <div>
+    <section id={slug}>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/blog/${slug}`} className="hover:underline">
           {title}
@@ -27,6 +27,6 @@ export default function PostPreview(
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
       <Avatar name={author.name} picture={author.picture} />
-    </div>
+    </section>
   );
 }
