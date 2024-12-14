44654f97a7ad5fb2477cf5e4d452d628247012b9
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-14 13:33:18 +0000

    Enhance TechIcon component to support tooltips for technology icons
    - Added tooltip functionality to the TechIcon component, allowing users to see additional context for each technology represented.
    - Integrated TooltipProvider, Tooltip, TooltipTrigger, and TooltipContent for improved user experience.
    - Updated the component to throw an error if the icon is not found, ensuring better error handling.
    
    These changes improve the interactivity and usability of the tech icons within the application.

--- src/components/tech_icon.tsx
+++ src/components/tech_icon.tsx
@@ -1,19 +1,42 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 import { TechDependency } from "@/interfaces/tech_dependency";
 import iconMap from "@/interfaces/icon_map";
 
 type Props = {
   icon: TechDependency
   applyHouseColour?: boolean
+  tooltip?: boolean
 }
 
-export default function TechIcon({ icon, applyHouseColour = false }: Props) {
-  const Icon = iconMap[icon];
-  if (!Icon) {
-    return <span>{icon}</span>;
+export default function TechIcon(
+  { icon, applyHouseColour = false, tooltip }: Props
+) {
+  const iconObject = iconMap[icon];
+  if (!iconObject) {
+    throw new Error(`Icon ${icon} not found`);
   }
-  return (
-    <Icon.icon
-      style={{ color: applyHouseColour ? Icon.houseColour : "inherit" }}
+  const iconElement = (
+    <iconObject.icon
+      style={{ color: applyHouseColour ? iconObject.houseColour : "inherit" }}
     />
+  )
+
+  if (!tooltip) {
+    return iconElement;
+  }
+
+  return (
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>{iconElement}</TooltipTrigger>
+        <TooltipContent>{iconObject.tooltip}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
