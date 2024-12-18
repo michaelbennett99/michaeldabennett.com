7f3f12f7ac560119049d9ab03613795132ca8320
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-18 11:35:20 +0000

    Enhance Dark Mode Toggle, fix hydration issues

--- src/app/_components/dark_mode_toggle.tsx
+++ src/app/_components/dark_mode_toggle.tsx
@@ -1,18 +1,45 @@
+import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-import { LuMoon, LuSun } from "react-icons/lu";
+import { LuSun, LuMoon, LuSunMoon } from "react-icons/lu";
 import NavButton from "./nav_button";
 
 export default function DarkModeToggle({ className }: { className?: string }) {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  const isDarkMode = theme === "dark";
-  const toggle = () => {
-    setTheme(isDarkMode ? "light" : "dark");
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
   }
+
+  const isSystem = theme === "system";
+  const isDarkMode = theme === "dark";
+  const isLightMode = theme === "light";
+  const toggle = () => {
+    if (isSystem) {
+      setTheme("light");
+    } else if (isLightMode) {
+      setTheme("dark");
+    } else if (isDarkMode) {
+      setTheme("system");
+    } else {
+      setTheme("system");
+    }
+  }
+
+  const icon = isLightMode
+    ? <LuSun />
+    : isDarkMode
+      ? <LuMoon />
+      : <LuSunMoon />;
 
   return (
     <NavButton onClick={toggle} className={className}>
-      {isDarkMode ? <LuMoon /> : <LuSun />}
+      {icon}
     </NavButton>
   )
 }
