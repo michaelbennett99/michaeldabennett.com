8342d18ad50d7379f63e1845f8a611d692633772
Michael Bennett <michael.d.a.bennett@outlook.com>
2025-01-23 14:56:53 +0000

    add list styles in project card

--- src/app/_cv/_components/project_card.tsx
+++ src/app/_cv/_components/project_card.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import markdownToHtml from "@/lib/markdownToHtml";
 import ProjectCardHeader from "@/components/projects/project_card_header";
 
 import CardHeader from "./card_header";
@@ -9,13 +11,26 @@ type Props = {
 }
 
 export default function ProjectCard({ project }: Props) {
+  const [content, setContent] = useState(project.content);
+
+  useEffect(() => {
+    async function processMarkdown() {
+      const htmlContent = await markdownToHtml(project.content);
+      setContent(htmlContent);
+    }
+    processMarkdown();
+  }, [project.content]);
+
   return (
     <div>
       <CardHeader className="[&_.project-title]:font-bold [&_.github-icon]:text-lg">
         <ProjectCardHeader project={project} />
       </CardHeader>
       <CardBody>
-        <p>{project.content}</p>
+        <div
+          className="[&>ul]:list-disc [&>ul]:list-inside [&>ul>li>ul]:ml-4 [&>ul>li>ul]:list-inside [&>ul>li>ul]:list-[circle]"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
       </CardBody>
     </div>
   );
--- src/app/_landing/_components/project_card.tsx
+++ src/app/_landing/_components/project_card.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { Project } from "@/interfaces/project";
+import { useEffect, useState } from "react";
+import markdownToHtml from "@/lib/markdownToHtml";
 
 import ProjectCardHeader from "@/components/projects/project_card_header";
 
@@ -7,12 +11,25 @@ type Props = {
 }
 
 export default function ProjectCard({ project }: Props) {
+  const [content, setContent] = useState(project.content);
+
+  useEffect(() => {
+    async function processMarkdown() {
+      const htmlContent = await markdownToHtml(project.content);
+      setContent(htmlContent);
+    }
+    processMarkdown();
+  }, [project.content]);
+
   return (
     <div
       className="space-y-2 [&_.project-title]:text-2xl [&_.project-title]:font-bold [&_.github-icon]:text-lg"
     >
       <ProjectCardHeader project={project} />
-      <p>{project.content}</p>
+      <div
+        className="[&>ul]:list-disc [&>ul]:list-outside [&>ul]:ml-4 [&>ul>li>ul]:list-[circle] [&>ul>li>ul]:ml-4 [&>ul]:ms-8"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </div>
   );
 }
