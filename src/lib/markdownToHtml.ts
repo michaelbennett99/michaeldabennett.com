fd846941158fca056504112d1c8bdd9dbf74e87b
Michael Bennett <michael.d.a.bennett@outlook.com>
2025-01-23 14:56:38 +0000

    add bullet processing

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -2,6 +2,12 @@ import { remark } from "remark";
 import html from "remark-html";
 
 export default async function markdownToHtml(markdown: string) {
-    const result = await remark().use(html).process(markdown);
+    const result = await remark()
+        .use(html, {
+            bullet: '-',
+            listItemIndent: '1',
+            sanitize: false
+        })
+        .process(markdown);
     return result.toString();
 }
